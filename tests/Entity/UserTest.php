6b30cf3ad4c3e653655bd8893c4bd991e5084962
Matheus Santos <matheus.santos@bsempresas.com.br>
2025-03-12 09:04:37 -0300

    chore: Update coverage report timestamps in HTML files

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testInitialValues(): void
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $user->getCreatedAt());
+        $this->assertNotNull($user->getCreatedAt());
+        $this->assertNull($user->getId());
+        $this->assertNull($user->getName());
+        $this->assertNull($user->getEmail());
+        $this->assertNull($user->getPassword());
+    }
+
+    public function testSettersAndGetters(): void
+    {
+        $user = new User();
+
+        $user->setName('John Doe');
+        $this->assertEquals('John Doe', $user->getName());
+
+        $user->setEmail('johndoe@example.com');
+        $this->assertEquals('johndoe@example.com', $user->getEmail());
+
+        $user->setPassword('password');
+        $this->assertEquals('password', $user->getPassword());
+    }
+
+    public function testConstructorSetsCreatedAt(): void
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(\DateTimeImmutable::class, $user->getCreatedAt());
+    }
+
+    public function testSetCreatedAt(): void
+    {
+        $user = new User();
+
+        $date = new \DateTimeImmutable('2023-03-12 10:00:00');
+        $user->setCreatedAt($date);
+
+        $this->assertEquals($date, $user->getCreatedAt());
+    }
+
+    public function testGettersAndSettersWithEmptyValues(): void
+    {
+        $user = new User();
+
+        $user->setName('');
+        $this->assertEquals('', $user->getName());
+
+        $user->setEmail('');
+        $this->assertEquals('', $user->getEmail());
+
+        $user->setPassword('');
+        $this->assertEquals('', $user->getPassword());
+    }
+}
